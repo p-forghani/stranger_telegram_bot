456447310aaf919595e6a76039e0b90bed55e794
pouria forghani <forghani.dev@gmail.com>
2024-01-07 21:32:32 +0300

    Add custom filters

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,12 @@
 import os
 
+import emoji
 import telebot
 from loguru import logger
-import emoji
 from telebot import types
 
 from constants import keyboards
+from utils.custom_filters import IsPouria
 from utils.io import read_json, write_json
 from utils.keyboard import create_keyboard
 
@@ -13,26 +14,37 @@ from utils.keyboard import create_keyboard
 class Bot:
     """Telegram bot to connect 2 strangers to randomly talk
     """
-    def __init__(self):
-        self.bot = telebot.TeleBot(os.environ['STRANGER_BOT_TOKEN'])
+    def __init__(self, bot_token):
+        # Build the bot
+        self.bot = telebot.TeleBot(bot_token)
+        # Add the logic to the bot
         self.handlers()
+        # Register the custom filters
+        self.bot.add_custom_filter(IsPouria())
         logger.info("Bot starting...")
+        # Run the bot
         self.bot.infinity_polling()
 
     def handlers(self):
         @self.bot.message_handler(func=lambda _: True)
-        def echo_all(message):
+        def echo_all(message: telebot.types.Message):
             """Send the user the message that recieve
 
             Args:
                 message (_type_): the message that user sends the bot
             """
-            write_json(message.json, 'message.json')
+            write_json(message.json, "data/message.json")
             self.bot.send_message(
                 message.chat.id,
                 message.text,
                 reply_markup=keyboards.main
             )
 
-if __name__ == "__main__":
-    bot = Bot()
+        @self.bot.message_handler(is_pouria=True)
+        def hi_pouria(message: telebot.types.Message):
+            """Say hi to Pouria
+
+            Args:
+                message (telebot.types.Message): _description_
+            """
+            self.bot.reply_to(message, 'Hi Pouria')
